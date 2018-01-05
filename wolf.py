06a85a76bc9c8f6541c77400e08cf36c1d695b14
Riley Martine <riley.martine@protonmail.com>
2018-01-05 13:41:46 -0500

    Modifications to wolf

--- wolf.py
+++ wolf.py
@@ -3,8 +3,13 @@ import wolframalpha
 client = wolframalpha.Client("TP9TEK-3EUL4W3A6L")
 
 #equation = input("Give me an equation: ")
-equation = "lim x->infinity x*e^(-2x)"
-res = client.query(f"{equation}")
-
+#equation = "lim x->infinity x*e^(-2x)"
+equation = "11x+18=172"
+params = (
+    ('podstate', 'Solution__Step-by-step+solution'),
+)
+res = client.query(f"{equation}", params=params)
+res2 = client.query(f"{equation}")
+print(res==res2)
 
 
