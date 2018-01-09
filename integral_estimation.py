30cea8cb2b23e149d08af0b15c88dd6fb34edd0b
Riley Martine <riley.martine@protonmail.com>
2018-01-09 14:39:41 -0500

    Make it easier to choose plot to draw

--- integral_estimation.py
+++ integral_estimation.py
@@ -6,9 +6,8 @@ import sys
 
 func = lambda t: ((1.4*e)**(0.02*t))+(cos(2*t))**2
 func = lambda t: -25*log(t+1)+120
-func = cos
-divisions = 50
-space = (0, pi)
+divisions = 6
+space = (0, 30)
 divsize = (space[1]-space[0])/divisions
 
 sum = 0
@@ -49,65 +48,69 @@ print("trap: " + str(round(sum, 4)))
 t = np.arange(space[0], space[1], 0.01)
 s = [func(x) for x in t]
 
-fig, ax = plt.subplots()
-ax.plot(t, s)
 
-mode = input("Which plot would you like? (lhs, rhs, mid, trap): ")
-
-if mode == "lhs":
-    # PLOT LHS
-    for divnum in range(divisions):
-        ax.add_patch(
-            patches.Rectangle(
-                (divnum*divsize, 0),   # (x, y)
-                divsize,               # width
-                func(divnum*divsize),  # height
-                fill=False,
+while True:
+    fig, ax = plt.subplots()
+    ax.plot(t, s)
+    mode = input("Which plot would you like? (lhs, rhs, mid, trap, quit): ")
+    
+    if mode == "lhs":
+        # PLOT LHS
+        for divnum in range(divisions):
+            ax.add_patch(
+                patches.Rectangle(
+                    (divnum*divsize, 0),   # (x, y)
+                    divsize,               # width
+                    func(divnum*divsize),  # height
+                    fill=False,
+                )
             )
-        )
-
-elif mode == 'rhs':  
-    #PLOT RHS
-    for divnum in range(divisions):
-        ax.add_patch(
-            patches.Rectangle(
-                (divnum*divsize, 0),       # (x, y)
-                divsize,                   # width
-                func((divnum+1)*divsize),  # height
-                fill=False,
+    
+    elif mode == 'rhs':  
+        #PLOT RHS
+        for divnum in range(divisions):
+            ax.add_patch(
+                patches.Rectangle(
+                    (divnum*divsize, 0),       # (x, y)
+                    divsize,                   # width
+                    func((divnum+1)*divsize),  # height
+                    fill=False,
+                )
             )
-        )
-
-elif mode == 'mid':   
-    #PLOT MIDPOINT
-    for divnum in range(divisions):
-        ax.add_patch(
-            patches.Rectangle(
-                (divnum*divsize, 0),   # (x, y)
-                divsize,               # width
-                func(divnum*divsize+0.5*divsize),  # height
-                fill=False,
+    
+    elif mode == 'mid':   
+        #PLOT MIDPOINT
+        for divnum in range(divisions):
+            ax.add_patch(
+                patches.Rectangle(
+                    (divnum*divsize, 0),   # (x, y)
+                    divsize,               # width
+                    func(divnum*divsize+0.5*divsize),  # height
+                    fill=False,
+                )
             )
-        )
-
-elif mode == 'trap':    
-    # PLOT TRAPEZOID
-    for divnum in range(divisions):
-        points = [
-                  ((divnum+1)*divsize, func((divnum+1)*divsize)),
-                  ((divnum+1)*divsize, 0),
-                  (divnum*divsize, 0),
-                  (divnum*divsize, func(divnum*divsize)),
-                 ]
-        ax.add_patch(
-            patches.Polygon(
-                points,
-                fill=False,
+    
+    elif mode == 'trap':    
+        # PLOT TRAPEZOID
+        for divnum in range(divisions):
+            points = [
+                      ((divnum+1)*divsize, func((divnum+1)*divsize)),
+                      ((divnum+1)*divsize, 0),
+                      (divnum*divsize, 0),
+                      (divnum*divsize, func(divnum*divsize)),
+                     ]
+            ax.add_patch(
+                patches.Polygon(
+                    points,
+                    fill=False,
+                )
             )
-        )
+    
+    elif mode == 'quit':
+        sys.exit(0)
 
-else:
-    print("Not a recognized mode. Goodbye!")
-    sys.exit(0)
-
-plt.show()
+    else:
+        print("Not a recognized mode.")
+        continue
+    
+    plt.show()
