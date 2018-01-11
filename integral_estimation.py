afcbf10fa1580a0fcb9aff530c400390d1f43014
Riley Martine <maxm215@gmail.com>
2018-01-11 15:01:49 -0500

    Add support for negative numbers  also there might be off by one errors

--- integral_estimation.py
+++ integral_estimation.py
@@ -1,4 +1,5 @@
-from math import sin, pi, cos, log, e
+"""Program for calculating Riemann sums of functions over intervals."""
+from math import sin, pi, cos, log, e, sqrt
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import numpy as np
@@ -6,46 +7,49 @@ import sys
 
 func = lambda t: ((1.4*e)**(0.02*t))+(cos(2*t))**2
 func = lambda t: -25*log(t+1)+120
-divisions = 6
-space = (0, 30)
-divsize = (space[1]-space[0])/divisions
+f1 = lambda x: 1-x**2
+func = lambda x: 0 if f1(x)<0 else np.sqrt(f1(x))
+func = sin
+divisions = 50
+space = (-1*pi, pi)
+divs, divsize = np.linspace(space[0], space[1], divisions, retstep=True)
 
 sum = 0
-for divnum in range(divisions):
-    sum += func(divnum*divsize)*divsize
+for div in divs:
+    sum += func(div)*divsize
 
 print("left hand sum: " + str(round(sum, 4)))
 
 
 sum = 0
-for divnum in range(divisions):
-    sum += func((divnum+1)*divsize)*divsize
+for div in divs:
+    sum += func(div+divsize)*divsize
 
 print("right hand sum: "+ str(round(sum, 4)))
 
 
 sum = 0
-for divnum in range(divisions):
-    midpoint = (divnum*divsize) + 0.5*divsize
+for div in divs:
+    midpoint = div + 0.5*divsize
     sum += func(midpoint)*divsize
 
 print("midpoint: " + str(round(sum, 4)))
 
 
 def trap_area(b1, b2, h):
-    return .5*(b1+b2)*h
+    return 0.5*(b1+b2)*h
 
 sum = 0
-for divnum in range(divisions):
-    b1 = func(divnum*divsize)
-    b2 = func((divnum+1)*divsize)
+for div in divs:
+    b1 = func(div)
+    b2 = func(div+divsize)
     h = divsize
     sum += trap_area(b1, b2, h)
 
-print("trap: " + str(round(sum, 4)))
+print("trap: " + str(round(sum, 4))) # can also find with avg(lhs, rhs) but that's not as fun
 
 
-t = np.arange(space[0], space[1], 0.01)
+t = np.linspace(space[0], space[1], 1000)
 s = [func(x) for x in t]
 
 
@@ -56,48 +60,48 @@ while True:
     
     if mode == "lhs":
         # PLOT LHS
-        for divnum in range(divisions):
+        for div in divs:
             ax.add_patch(
                 patches.Rectangle(
-                    (divnum*divsize, 0),   # (x, y)
+                    (div, 0),   # (x, y)
                     divsize,               # width
-                    func(divnum*divsize),  # height
+                    func(div),  # height
                     fill=False,
                 )
             )
     
     elif mode == 'rhs':  
         #PLOT RHS
-        for divnum in range(divisions):
+        for div in divs:
             ax.add_patch(
                 patches.Rectangle(
-                    (divnum*divsize, 0),       # (x, y)
+                    (div, 0),       # (x, y)
                     divsize,                   # width
-                    func((divnum+1)*divsize),  # height
+                    func(div+divsize),  # height
                     fill=False,
                 )
             )
     
     elif mode == 'mid':   
         #PLOT MIDPOINT
-        for divnum in range(divisions):
+        for divstart in divs:
             ax.add_patch(
                 patches.Rectangle(
-                    (divnum*divsize, 0),   # (x, y)
+                    (divstart, 0),   # (x, y)
                     divsize,               # width
-                    func(divnum*divsize+0.5*divsize),  # height
+                    func(divstart+0.5*divsize),  # height
                     fill=False,
                 )
             )
     
     elif mode == 'trap':    
         # PLOT TRAPEZOID
-        for divnum in range(divisions):
+        for divstart in divs:
             points = [
-                      ((divnum+1)*divsize, func((divnum+1)*divsize)),
-                      ((divnum+1)*divsize, 0),
-                      (divnum*divsize, 0),
-                      (divnum*divsize, func(divnum*divsize)),
+                      (divstart+divsize, func(divstart+divsize)),
+                      (divstart+divsize, 0),
+                      (divstart, 0),
+                      (divstart, func(divstart)),
                      ]
             ax.add_patch(
                 patches.Polygon(
